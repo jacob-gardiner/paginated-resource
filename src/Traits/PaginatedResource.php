529b5fe86d6741e61e7279891eac23c9651cceb8
Jacob Gardiner <jacob_gardiner@hotmail.ca>
2020-06-23 15:23:20 -0400

    Update composer file

--- src/Traits/PaginatedResource.php
+++ src/Traits/PaginatedResource.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Jacobgardiner\Paginatedresource\Traits;
+namespace JacobGardiner\PaginatedResource\Traits;
 
 trait PaginatedResource
 {
@@ -8,18 +8,18 @@ trait PaginatedResource
     {
         $lastPage = $this->lastPage();
         return [
-            'total'          => $this->total(),
-            'count'          => $this->count(),
-            'per_page'       => $this->perPage(),
-            'current_page'   => $this->currentPage(),
-            'total_pages'    => $lastPage,
-            'next_page_url'  => $this->nextPageUrl(),
-            'prev_page_url'  => $this->previousPageUrl(),
+            'total' => $this->total(),
+            'count' => $this->count(),
+            'per_page' => $this->perPage(),
+            'current_page' => $this->currentPage(),
+            'total_pages' => $lastPage,
+            'next_page_url' => $this->nextPageUrl(),
+            'prev_page_url' => $this->previousPageUrl(),
             'first_page_url' => $this->url(1),
-            'last_page_url'  => $this->url($lastPage),
-            'path'           => $this->path(),
-            'from'           => $this->firstItem(),
-            'to'             => $this->lastItem(),
+            'last_page_url' => $this->url($lastPage),
+            'path' => $this->path(),
+            'from' => $this->firstItem(),
+            'to' => $this->lastItem(),
         ];
     }
 }
